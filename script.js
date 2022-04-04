382fc71b1e894f9187270812682eca2866b77632
Kevin <kbpeah@gmail.com>
2022-04-04 19:13:21 -0400

    Restructured. User input is validated. User & Computer choice is made but not shown.

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-//--------------------------DECLARATIONS-----------------------------
-const playerSelection = playerPlay();
-const computerSelection = computerPlay();
-
-//--------------------------FUNCTIONS--------------------------------
-function playerPlay() {
-    playerChoice = prompt ("Type Rock, Paper, or Scissors"); {
-        if (playerChoice != "Rock");
-
+let playGame = confirm ("Play Rock, Paper, Scissors?")
+if (playGame) {
+    let playerChoice = prompt ("Pick Rock, Paper, or Scissors");
+    if (playerChoice) {
+        let playerOne = playerChoice.trim().toLowerCase();
+        if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
+            
+            let computerChoice = Math.floor(Math.random() * 3 + 1); //without the +1 it would generate a random number from 0 - 2 instead of 1 - 3
+            let computer = computerChoice === 1 ? "rock"
+                : computerChoice === 2 ? "paper"
+                : "scissors";
+                
+        } else {
+            alert ("You didn't choose Rock, Paper, or Scissors");
+        }
+    } else {
+        alert ("Game cancelled");
     }
+} else {
+    alert ("Game cancelled");
 }
-
-function computerPlay() {
-    computerChoice = Math.floor(Math.random() * 3)
-    if (computerChoice == 0)
-        var computerChoice = "Rock"
-    else if (computerChoice == 1)
-        var computerChoice = "Paper"
-    else var computerChoice = "Scissors"
-
-console.log ("The Computer chose " + computerChoice);
-}
-
-//--------------------------Calling the Functions--------------------
-
-
-
-
-//-------------------------------------------------------------------
